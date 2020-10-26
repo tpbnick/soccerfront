c92a24777f88df330ba7f8ee13caecb25d196d79
tpbnick <nickplatt4@gmail.com>
2020-10-26 17:18:36 -0400

    commit 10/26 2

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/* api-football key a2994c55ccmsh52c97e939e28026p1ac42djsn3342553e6cbb
+https://rapidapi.com/api-sports/api/api-football-beta
+*/
+
 const express = require('express');
 const app = express();
 const path = require('path');
