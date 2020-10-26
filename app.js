898de6da451ab937c9db302487a7743cd652ca87
tpbnick <49327729+tpbnick@users.noreply.github.com>
2020-10-26 13:58:46 -0400

    commit 10/26

--- app.js
+++ app.js
@@ -7,18 +7,6 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) =>{
 	res.render('home')
-var request = require("request");
-
-var options = {
-  method: 'GET',
-  url: 'https://api-football-beta.p.rapidapi.com/leagues',
-  qs: {id: '61'},
-  headers: {
-    'x-rapidapi-host': 'api-football-beta.p.rapidapi.com',
-    'x-rapidapi-key': 'a2994c55ccmsh52c97e939e28026p1ac42djsn3342553e6cbb',
-    useQueryString: true
-  }
-};
 
 request(options, function (error, response, body) {
 	if (error) throw new Error(error);
@@ -26,18 +14,6 @@ request(options, function (error, response, body) {
 	console.log(body);
 });
 
-
-request(options, function (error, response, body) {
-	if (error) throw new Error(error);
-
-	console.log(body);
-});
-
-request(options, function (error, response, body) {
-	if (error) throw new Error(error);
-
-	console.log(body);
-});
 
 })
 
