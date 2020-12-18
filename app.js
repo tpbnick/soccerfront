0a09f6fa0697668ac1b72987afdd932cc4171c1b
tpbnick <nickplatt4@gmail.com>
2020-12-17 22:43:15 -0500

    Commit 12/17 - 2
    Added dotenv library.

--- app.js
+++ app.js
@@ -2,29 +2,29 @@
 https://rapidapi.com/api-sports/api/api-football-beta
 */
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const favicon = require('serve-favicon');
+const path = require("path");
+const favicon = require("serve-favicon");
 
-app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
-app.use(express.static('public'));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', function(req, res){	
-	res.render('home');
-})
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
-app.get('/league-tables', function(req, res){
-	res.render('league-tables');
-})
+app.get("/league-tables", function (req, res) {
+  res.render("league-tables");
+});
 
-app.get('/login', function(req, res){
-	res.render('login');
-})
+app.get("/login", function (req, res) {
+  res.render("login");
+});
 
 app.listen(8080, () => {
-	console.log('Serving on port 8080')
-})
+  console.log("Serving on port 8080");
+});
